d9d1e15879ae76de6b5c23acb5539d4d5e855ebc
Salome Aufrecht <aufrecht.sla@web.de>
2024-07-17 10:31:32 +0200

    example roxygen2 comments

---
+++ R/costComplexityPruning.R
@@ -0,0 +1,8 @@
+#' this is an example documentation
+#' 
+#' there are more tags...
+#' compile with devtools::document()
+#' @export #visible from outside
+#' @param x whatever
+#' 
+foo2 <- function(x) x
---
+++ R/datastructure_tree.R
@@ -0,0 +1,9 @@
+
+#' this is an example documentation
+#' 
+#' there are more tags...
+#' compile with devtools::document()
+#' @export #visible from outside
+#' @param x whatever
+#' 
+foo <- function(x) x

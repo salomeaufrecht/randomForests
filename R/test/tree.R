86c65a80f3952da6daa36ddd1a75651181956af5
BADAKAA <81486945+BADAKAA@users.noreply.github.com>
2024-07-26 21:54:42 +0200

    refactor and extend tree_datastructure.R
    - replace NaN with NA
    - remove length attribute
    - add (recursive) delete function
    - update exists function: the index is now always defined, so the exists function now checks the values instead of the index for NA
    - update bracket operator and get_children return types
    -overload assignment operator
    - create "exetend" function to double matrix size
    - use assignment operator in add_children method

--- R/test/tree.R
+++ R/test/tree.R
@@ -2,10 +2,29 @@ source("R/tree_datastructure.R")
 
 testTreeDatastructure = function() {
   cat("Testing Tree datastructure...\n")
-  test <- Tree$new(length=10)
-  element5 <- test[5]
-  print(element5)
-  cat(paste0("Index of element 5: ", test[5][1]))
+  test <- Tree$new(len=3)
+
+  
+  print("Assign element 1 & 2. Element 1:")
+  test[1] <- c(5,6)
+  test[2] <- c(5324,631)
+  print(test[1])
+  cat("\nShould be: 1, 5, 6\n\n")
+  cat("Element at index: 5\n")
+  print(test[5])
+  cat("Should be: NA\n\n")
+
+  cat("Test mass access. Print elements at index two through five:\n")
+  print(test[c(2,3,5)])
+
+  cat(paste0("\nTest mass assignment and automatic extension. Length before: ",nrow(test$data),"\n"))
+  test[c(3,4,5,6,7)] = rep(list(c(1,1)),5)
+  cat("Inserted elements (should all have j=1 and s=1):\n")
+  print(test[c(3,4,5,6,7)])
+  cat(paste0("\nLength after: ",nrow(test$data), "Should be: 8\n\n"))
+
+
+  cat(paste0("Index of element 2: ", test[5][1]))
   cat("\nShould be: 5\n\n")
   cat(paste0("Depth of element 3: ",test$depth(3)))
   cat("\nShould be: 2\n\n")
@@ -13,10 +32,12 @@ testTreeDatastructure = function() {
   cat("\nShould be: FALSE\n\n")
   cat(paste0("Is element 6 a leaf? ",test$is_leaf(6)))
   cat("\nShould be: TRUE\n\n")
+
   cat("\nGive children to 6\n")
   test$add_children(6, 1, 1, 2, 2)
   cat("\nChildren of 6: \n")
   print(test$get_children(6))
+
 }
 testTreeDatastructure()
 
--- R/tree_datastructure.R
+++ R/tree_datastructure.R
@@ -14,17 +14,15 @@ Tree <- setRefClass(
   "Tree",
   fields = list(
     data = "matrix",
-    length = "numeric",
     d = "numeric",
     type = "character"
   ),
   methods = list(
-    initialize = function(length=16, max_length = 1024, dimension = 50, classificationType = FALSE) {
-      stopifnot(length >= 1)
+    initialize = function(length, dimension = 50, classificationType = FALSE) {
+      length <- 2 ^ ceiling(log2(length))
         # 1. Entry = Index, 2. Entry = j, 3. Entry = s
-      .self$data <- matrix(rep(NaN, max_length*3), ncol=3)
+      .self$data <- matrix(rep(NA, length*3), ncol=3)
       .self$data[1:length, 1] <- 1:length
-      .self$length <- length
       .self$type <- ifelse(classificationType, "classification", "regression")
       .self$d <- dimension
     },
@@ -32,58 +30,62 @@ Tree <- setRefClass(
     depth = function(index) floor(log2(index)) + 1,
     
     exists = function(index) {
-        if(index <= nrow(.self$data) && !is.nan(.self$data[index,1])) {
-            return(TRUE)
-        }
-        #warning("Node does not exist")
-        return(FALSE)
+      if (length(index) > 1) return(.self$all_exist(index))
+      index <= nrow(.self$data) && (!is.na(.self$data[index, 2]) || !is.na(.self$data[index, 3]))
     },
-    
+    all_exist = function(indices) all(sapply(indices,.self$exists)),
+
+    has_parent = function (index) ifelse(index==1, TRUE, !is.na(.self$data[floor(index * 0.5),2])),
+
+    get_parent = function(index) .self[floor(index * 0.5)],
+
+    extend = function(length_new) {
+      max_length <- nrow(.self$data)
+      if (length_new <= max_length) return()
+      # TODO: Discuss if this warning is necessary
+      # warning("Max length exceeded. More space will be allocated.")
+      length_new <- 2 ^ ceiling(log2(length_new))
+      new_matrix <- matrix(rep(NA, 3*length_new), ncol=3)
+      new_matrix[1:max_length, ] <- .self$data[1:max_length, ]
+      new_matrix[1:length_new, 1] <- 1:length_new
+      .self$data <- new_matrix
+    },
+  
+    get_child_indices = function(index) {
+      if (!.self$exists(index)) stop("Parent node does not exist")
+      first_child_index <- index * 2
+      indices <- NA
+      if (.self$exists(first_child_index)) indices <- first_child_index
+      if (.self$exists(first_child_index+1)) indices <- c(indices, first_child_index+1)
+      if (all(is.na(indices))) warning("Node is leaf node")
+      return(indices)
+    },
+  
     get_children = function(index) {
-        return(
-            .self$data[.self$get_children_index(index), ]
-        )
+        indices = .self$get_child_indices(index)
+        .self[indices]
     },
     
-    get_children_index = function(index) {
-      .self$exists(index)
-      first_child_index <- index*2
-      children <- NULL
-      if(.self$exists(first_child_index)) {
-          children <- first_child_index
-      }
-      if(.self$exists(first_child_index+1)) {
-          children <- c(children, first_child_index+1)
-      }
-      if(is.null(children)) warning("Node is leaf node")
-      return(children)
-    },
-    
-    add_children = function(index, j1, s1, j2=NaN, s2=NaN) {
+    add_children = function(index, j1, s1, j2=NA, s2=NA) {
         stopifnot(all(sapply(c(j1, s1, j2, s2), is.numeric)))
         max_length <- nrow(.self$data)
-        if(index+2 > max_length) {
-           warning("Matrix exceeds max length. More space will be allocated")
-           new_matrix <- matrix(1:max_length*2, ncol=3)
-           new_matrix[1:max_length, ] <- .self$data[1:max_length, ]
-           max_length <- max_length*2
-           .self$data <- new_matrix
-        } 
-        
-        .self$data[index*2, ] <- c(index*2, j1, s1)
-        .self$length = .self$length+1
-        if(!(is.na(j2) && is.na(s2))) {
-            .self$data[index*2 + 1, ] <- c(index*2 + 1, j2, s2)
-            .self$length = .self$length+1
+        .self[index*2] <- c(j1, s1)
+        if (!is.na(j2) || !is.na(s2)) {
+            .self[index*2 + 1] <- c(j2, s2)
         }
-        
     },
-    get_parent = function(index) {
-      .self$data[floor(index * 0.5)]
+
+    delete = function(index) {
+      if (!.self$exists(index)) return()
+      children = .self$get_children(index)
+      .self$data[index, ] <- c(index,NA,NA)
+      # Delete child nodes recursively
+      if (!is.na(children[1])) .self$delete(children[1]$index)
+      if (!is.na(children[2])) .self$delete(children[2]$index)
     },
     
     is_leaf = function(index) {
-        stopifnot(.self$exists(index))
+        if (!.self$exists(index)) stop("Node does not exist.")
         return(!.self$exists(index*2))
     },
     
@@ -98,15 +100,50 @@ Tree <- setRefClass(
 #' Allows accessing the n-th element of the tree (equivalent to array representation)
 #' getting child and parents elements, determining whether 
 #' 
+#' https://www.geeksforgeeks.org/array-representation-of-binary-heap/
+#' 
 #' @export `[.Tree`
-#' @param x tree
+#' @param tree tree
 #' @param value index of element
 #' 
 #' 
 
-`[.Tree` <- function(x, value) {
-    stopifnot(x$exists(value))
-    x$data[value, ]
+`[.Tree` <- function(tree, value) {
+    if (length(value) > 1) return(lapply(value, function(x) tree[x]))
+    if (value > nrow(tree$data)) return(NA)
+    el <- tree$data[value, ]
+    if((length(el) == 1 && is.na(el)) || is.na(el[2])) return(NA)
+    return(c(index=el[1], j=el[2], s=el[3]))
+}
+
+#' Allows writing to the the n-th element of the tree.
+#' Vectorised mass assignment possible.
+#' Passing NA as a value will result in the deletion of a node and all its children.
+#' 
+#' @export `[<-.Tree`
+#' @param tree tree
+#' @param index index of element
+#' @param value atomic vector of two real numbers (j, s)
+#' 
+#' 
+`[<-.Tree` <- function(tree, index, value) {
+  if (length(value) == 1 && is.na(value)) value = c(NA, NA)
+  if (length(index) > 1) {
+      if (!is.list(value)) stop("Mass assignment is only possible using lists of vectors.")
+      sapply(1:length(index), function(x) tree[index[x]] <- value[[x]])
+      return(tree)
+  }
+  if (!tree$has_parent(index)) stop("Parent does not exist!")
+  if (!is.numeric(value) || !is.atomic(value) || length(value) != 2) {
+    stop("Only atomic vectors of length two (j, s) are permitted as values.")
+  }
+  if (all(is.na(value))) {
+    tree$delete(value)
+    return(tree)
+  }
+  tree$extend(index)
+  tree$data[index, ] <- c(index,value)
+  tree
 }
 
 setMethod(f = "show",
@@ -114,7 +151,7 @@ setMethod(f = "show",
           definition = function(object) {
             cat("Tree\n")
             print(c(type=object$type,
-                    length=object$length,
+                    nodes=sum(where(!is.na(object$data[,2]))),
                     depth=object$d,
                     dimension=object$d),
             )
@@ -128,4 +165,3 @@ setMethod(f = "show",
 #' 
 #' 
 print.Tree <- function(x) show(x)
-

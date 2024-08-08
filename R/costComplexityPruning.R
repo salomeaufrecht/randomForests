d2b2c295817b2ab27e09b5d999e58c360a17b893
Salome Aufrecht <aufrecht.sla@web.de>
2024-08-08 17:10:29 +0200

    add descriptions

--- R/costComplexityPruning.R
+++ R/costComplexityPruning.R
@@ -1,12 +1,20 @@
 
-#' this is an example documentation
-#' 
-#' there are more tags...
-#' compile with devtools::document()
-#' @export costComplexityPruning
-#' @param x whatever
-#' 
-costComplexityPruning <- function(t, lambda_min=1, lambda_max=100, lambda_step=1, plot=FALSE, m=10, print_progress=FALSE){
+#' Cost-Complexity Pruning
+#'
+#' This function performs cost-complexity pruning on a decision tree using cross-validation to select the optimal complexity parameter (\eqn{\lambda}).
+#'
+#' @param t The original decision tree to be pruned.
+#' @param m Number of partitions to create for cross-validation. Default is 10.
+#' @param lambda_min Minimum value for the complexity parameter \eqn{\lambda}. Default is 1.
+#' @param lambda_max Maximum value for the complexity parameter \eqn{\lambda}. Default is 100.
+#' @param lambda_step Step size for iterating over \eqn{\lambda} values. Default is 1.
+#' @param plot Logical flag to indicate whether to plot the cross-validation error against \eqn{\lambda}. Default is FALSE.
+#' @param print_progress Logical flag to indicate whether to print progress messages during execution. Default is FALSE.
+#' @return A pruned decision tree based on the optimal \eqn{\lambda} value.
+#' @examples
+#' # Assuming `tree` is an initialized decision tree object
+#' pruned_tree <- costComplexityPruning(tree, lambda_min=1, lambda_max=50, plot=TRUE)
+costComplexityPruning <- function(t, m=10, lambda_min=1, lambda_max=100, lambda_step=1, plot=FALSE,  print_progress=FALSE){
     
     stopifnot("m need to be greater 1"= m>=2)
     Im <- makePartition(t$training_data_x, m)
@@ -40,7 +48,18 @@ costComplexityPruning <- function(t, lambda_min=1, lambda_max=100, lambda_step=1
     return(chooseTpLambda(lambda, getPruningSequence(t)))
 }
 
-
+#' Cross-Validation Error Calculation
+#'
+#' Calculates the cross-validation error for a given complexity parameter \eqn{\lambda} 
+#' by pruning a decision trees with different training data and evaluating its performance on validation sets.
+#'
+#' @param lambda The complexity parameter for pruning.
+#' @param sequences A list of pruning sequences generated from different training data.
+#' @param n The number of observations in the complete training data.
+#' @param Im The partition indices for cross-validation.
+#' @param training_data_x The training data.
+#' @param training_data_y The training data.
+#' @return The cross-validation error for the given \eqn{\lambda}.
 CV <- function(lambda, sequences, n, Im, training_data_x, training_data_y){
     sum <- 0
     for (m in seq_along(sequences)){
@@ -54,6 +73,16 @@ CV <- function(lambda, sequences, n, Im, training_data_x, training_data_y){
     return((1/n) *sum)
 }
 
+#' Loss Function
+#'
+#' Computes the loss between the true label and the predicted label based on the type of tree (classification or regression).
+#'
+#' @param t The decision tree object.
+#' @param y The true label.
+#' @param s The predicted label.
+#' @return The computed loss value.
+#' @examples
+#' loss <- L(tree, y_true, y_pred)
 L <- function(t, y, s){
     if (t$type == "classification"){
         return(ifelse(y!=s, 1, 0))
@@ -61,6 +90,13 @@ L <- function(t, y, s){
     return((y-s)^2)
 }
 
+#' Create Cross-Validation Partitions
+#'
+#' Creates a partition of the training data into `m` parts for cross-validation purposes.
+#'
+#' @param x The training data.
+#' @param m The number of partitions to create. Default is 10.
+#' @return A vector indicating the partition assignments for each observation.
 makePartition <- function(x, m=10){
     n <- length(x)
     size <- round(n/m)
@@ -72,10 +108,17 @@ makePartition <- function(x, m=10){
     return(part)
 }
 
+#' Choose Optimal Subtree for Given \eqn{\lambda}
+#'
+#' Selects the subtree from the pruning sequence that minimizes the 
+#' cost-complexity criterion for a given \eqn{\lambda}.
+#'
+#' @param lambda The complexity parameter for pruning.
+#' @param pruningSequence A list of subtrees generated during the pruning process.
+#' @return The subtree that minimizes the cost-complexity criterion.
 chooseTpLambda <- function(lambda, pruningSequence){
     min_value <- .Machine$integer.max
     tLambda <- NULL
-    print(pruningSequence)
     for(t in pruningSequence){
         val <- t$calcRisk() + lambda * length(t$get_leaves())
         if(val<min_value){
@@ -87,12 +130,12 @@ chooseTpLambda <- function(lambda, pruningSequence){
 }
 
 
-#' Pruning Sequence
-#' 
-#' calculates trees t(0), ..., t(p) according to weakest link pruning
-#' 
-#' @param t original tree
-#' @return list of trees t(0), ..., t(p)
+#' Generate Pruning Sequence
+#'
+#' Generates a sequence of subtrees using weakest link pruning from the original tree.
+#'
+#' @param t The original decision tree.
+#' @return A list of subtrees representing the pruning sequence (t(0), ..., t(p)).
 getPruningSequence <- function(t){
     possibleTrees <- getSubTrees(t)
 
@@ -101,13 +144,15 @@ getPruningSequence <- function(t){
 
 
 #' Recursive Pruning Sequence
+#'
+#' Should not be used by itself!
 #' 
-#' Recursive helper function for getPruningSequence(t)
-#' Not to be used other than in getPruningSequence(t)
-#' 
-#' @param t0 tree
-#' @param xMask see getXMask(t)
-#' @param possibleTrees list of subtrees of t0
+#' A helper function that recursively generates the pruning sequence for the given tree.
+#'
+#' @param t0 The current tree in the pruning process.
+#' @param possibleTrees A list of possible subtrees that can be generated from the current tree.
+#' @return A list of subtrees in the pruning sequence.
+
 recPruningSequence <- function(t0, possibleTrees){
     if(t0$is_leaf(1)) return()
     leaf_count_old <- length(t0$get_leaves())
@@ -130,15 +175,14 @@ recPruningSequence <- function(t0, possibleTrees){
     return(c(list(tp), recPruningSequence(tp, possibleTrees)))
 }
 
-#' get remaining Subtrees
-#' 
-#' given a set of subtrees and a specific subtree t0 from this set. we now calculate 
-#' a subset of the subtrees in which all subtrees are subtrees of t0
-#' 
-#' @param t0 given tree
-#' @param possibleTrees set of all subtrees.
-#' @return set of all remaining subtrees (as list)
-#' 
+#' Get Remaining Subtrees
+#'
+#' Filters the list of possible subtrees to include only those that are
+#' subtrees of a specified tree.
+#'
+#' @param t0 The reference tree to compare against.
+#' @param possibleTrees A list of all possible subtrees.
+#' @return A filtered list of subtrees that are subtrees of `t0`.
 getRemainingSubtrees <- function(t0, possibleTrees ){
     nodes <- c(1,  t0$data[!is.na(t0$data[, 'y']), 'index'])
     delTrees <- NULL
@@ -153,11 +197,12 @@ getRemainingSubtrees <- function(t0, possibleTrees ){
 
 
 
-#' Get possible subtrees
-#' 
-#' calculates all possible subtrees with same root like given tree (needed for pruning)
-#' @param t tree
-#' @return vector of subtrees 
+#' Generate Possible Subtrees
+#'
+#' Generates all possible subtrees of the original tree that share the same root.
+#'
+#' @param t The original decision tree.
+#' @return A vector of subtrees.
 getSubTrees <- function(t){
     t_sub <- Tree$new(t$training_data_x, t$training_data_y)
     t_sub$type <- t$type
@@ -169,10 +214,16 @@ getSubTrees <- function(t){
     return(subtrees)
 }
 
-#' recursive helper function for getSubTree
-#' @param t_original original tree
-#' @param t_sub subtree currently working on (either append child or don't)
-#' @return list of subtrees
+#' Recursive Subtree Generation
+#'
+#'Should not be used by itself!
+#'
+#' A helper function that recursively generates all possible subtrees of a given tree.
+#'
+#' @param t_original The original decision tree.
+#' @param t_sub The current subtree in the generation process.
+#' @return A list of subtrees generated from `t_original`.
+
 recSubTrees <- function(t_original, t_sub){
     leaves <- t_sub$get_leaves()
     for (l in leaves){
